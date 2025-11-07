5c94496fee73fe41ea286592c2887d3d63ac091c
Nimira43 <n.rabone72@aol.com>
2025-11-07 19:54:01 +0000

    Installed Keycloak

--- Sophon/Sophon.ServiceDefaults/obj/Debug/net9.0/Sophon.ServiceDefaults.AssemblyInfo.cs
+++ Sophon/Sophon.ServiceDefaults/obj/Debug/net9.0/Sophon.ServiceDefaults.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sophon.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cea098919c7e481280c8fa9dbcbbde90680b0261")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sophon.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sophon.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
